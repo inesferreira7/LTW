8594c8d9087f414307aa376046918ba538307fd1
Ines Ferreira <inesferreira789@gmail.com>
2016-11-09 11:19:08 +0000

    Added tp3 - incomplete

---
+++ tp3/sum2.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+$sum = $num1 + $num2;
+
+?>
+
+<body>
+<h1> <?=$sum?> </h1>
+<form action="form2.html"><input type="submit" value="Back"></form>
+</body>
