aab2981dc78e8ce437e2da58c09091bbefa8c620
Ines Ferreira <inesferreira789@gmail.com>
2016-11-27 14:44:57 +0000

    Button register

--- proj/principal/script.js
+++ proj/principal/script.js
@@ -8,12 +8,7 @@ $(document).ready(function() {
             $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
             $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
             $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
-
-
-
-
-
-
+            $form.append('<button type="button" class="button"> Register! </button');
 
             $('body').append($form);
         }
