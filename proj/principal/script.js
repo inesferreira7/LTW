82a07767ec8a8e2fa2a66e85d10e660d15c45062
Ines Ferreira <inesferreira789@gmail.com>
2016-11-27 14:59:21 +0000

    Duplicated forms problem resolved

--- proj/principal/script.js
+++ proj/principal/script.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
 
   $('input:radio[value="user"]').change(function(){
-        if ($(this).is(':checked') == true) {
-            $form = $("<form ></form>");
-            $form.append('<label> First name <input type="text" class ="preenche" name="firstname"></label><br>'); //Primeiro nome
-            $form.append('<label> Last name<input type="text" class ="preenche" name="lastname"></label><br>'); //Ultimo nome
-            $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
-            $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
-            $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
-            $form.append('<button type="button" class="button"> Register! </button');
+    if ($(this).is(':checked') == true) {
+      if($('form').length != 0){ //Impede que quando voltamos a carregar crie outro abaixo
+        $('form').remove();
+      }
+      $form = $("<form ></form>");
+      $form.append('<label> First name <input type="text" class ="preenche" name="firstname"></label><br>'); //Primeiro nome
+      $form.append('<label> Last name<input type="text" class ="preenche" name="lastname"></label><br>'); //Ultimo nome
+      $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
+      $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
+      $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
+      $form.append('<button type="button" class="button"> Register! </button');
 
-            $('body').append($form);
-        }
-      });
+      $('body').append($form);
+    }
+  });
+
+  $('input:radio[value="owner"]').change(function(){
+    if ($(this).is(':checked') == true) {
+      if($('form').length != 0){
+        $('form').remove();
+      }
+      $form = $("<form ></form>");
+      $form.append('<label> First name <input type="text" class ="preenche" name="firstname"></label><br>'); //Primeiro nome
+      $form.append('<label> Last name<input type="text" class ="preenche" name="lastname"></label><br>'); //Ultimo nome
+      $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
+      $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
+      $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
+      $form.append('<button type="button" class="button"> Register! </button');
+
+      $('body').append($form);
+    }
+  });
 
   });
