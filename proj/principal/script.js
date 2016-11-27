9eca7dba95ae9d40971f4170b1a3c6144b9d7488
Ines Ferreira <inesferreira789@gmail.com>
2016-11-27 14:26:52 +0000

    When user wants to register as a reviewer, appears proper form

--- proj/principal/script.js
+++ proj/principal/script.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-  $('input:radio[name="type"]').change(function(){
+  $('input:radio[value="user"]').change(function(){
         if ($(this).is(':checked') == true) {
-            $('#header').append('<div>hello world</div>');
+            $form = $("<form ></form>");
+            $form.append('<label> First name <input type="text" class ="preenche" name="firstname"></label><br>'); //Primeiro nome
+            $form.append('<label> Last name<input type="text" class ="preenche" name="lastname"></label><br>'); //Ultimo nome
+            $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
+            $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
+            $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
+
+
+
+
+
+
+
+            $('body').append($form);
         }
       });
 
