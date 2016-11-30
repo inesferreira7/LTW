db4ddc4359fd87a1f1d1a471082be152434da4c7
Ines Ferreira <inesferreira789@gmail.com>
2016-11-30 15:11:58 +0000

    Added interface relative with restrictions

--- proj/principal/script.js
+++ proj/principal/script.js
@@ -9,11 +9,28 @@ $(document).ready(function() {
       $form.append('<label> First name <input type="text" class ="preenche" name="firstname"></label><br>'); //Primeiro nome
       $form.append('<label> Last name<input type="text" class ="preenche" name="lastname"></label><br>'); //Ultimo nome
       $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
-      $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
-      $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
+      $form.append('<div id="un"><label> Username <input type="text" class ="preenche" name="username"></label><img class="u" src ="info.png" alt="inf" width="25" height="25"></div><br>'); //Username
+      $form.append('<div id="pw"><label> Password <input type="password" class ="preenche" name="password"></label><img class="p" src ="info.png" alt="inf" width="25" height="25"></div><br>'); //Password
       $form.append('<button type="submit" class="button"> Register! </button>');
 
       $('body').append($form);
+
+        $( "img[class='u']" ).mouseover(function() {
+          $( "#un" ).append( "<p> Username needs to be at least 3 characters long.</p>" );
+        });
+
+        $( "img[class='u']" ).mouseout(function() {
+          $( "p" ).remove();
+        });
+
+        $( "img[class='p']" ).mouseover(function() {
+          $( "#pw" ).append( "<p> Password needs to be at least 7 characters long.</p>" );
+        });
+
+        $( "img[class='p']" ).mouseout(function() {
+          $( "p" ).remove();
+        });
+
     }
   });
 
@@ -33,10 +50,6 @@ $(document).ready(function() {
       $('body').append($form);
     }
   });
-
-  function validateForm(){
-    return true;
-  }
 
   });
 
