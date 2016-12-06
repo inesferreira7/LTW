a9adb95dfefef0d474c2ad08f2695e72e5140497
JoaoChaves96 <joaovitor1906@hotmail.com>
2016-12-06 15:07:53 +0000

    script.js updated for owner register

--- proj/principal/js/script.js
+++ proj/principal/js/script.js
@@ -39,15 +39,31 @@ $(document).ready(function() {
       if($('form').length != 0){
         $('form').remove();
       }
-      $form = $("<form ></form>");
+      $form = $('<form id="form" method="post" action="../pages/signupO.php"></form>');
       $form.append('<label> First name <input type="text" class ="preenche" name="firstname"></label><br>'); //Primeiro nome
       $form.append('<label> Last name<input type="text" class ="preenche" name="lastname"></label><br>'); //Ultimo nome
       $form.append('<label> Email <input type="e-mail" class ="preenche" name="email"></label><br>'); //Email
-      $form.append('<label> Username <input type="text" class ="preenche" name="username"></label><br>'); //Username
-      $form.append('<label> Password <input type="password" class ="preenche" name="password"></label><br>'); //Password
+      $form.append('<div id="un"><label> Username <input type="text" class ="preenche" name="username"></label><img class="u" src ="../res/images/info.png" alt="inf" width="25" height="25"></div><br>'); //Username
+      $form.append('<div id="pw"><label> Password <input type="password" class ="preenche" name="password"></label><img class="p" src ="../res/images/info.png" alt="inf" width="25" height="25"></div><br>'); //Password
       $form.append('<button type="button" class="button"> Register! </button>');
 
       $('body').append($form);
+
+        $( "img[class='u']" ).mouseover(function() {
+          $( "#un" ).append( "<h5 id='userParagraph' > Username needs to be at least 3 characters long.</h5>" );
+        });
+
+        $( "img[class='u']" ).mouseout(function() {
+          $( "h5" ).remove();
+        });
+
+        $( "img[class='p']" ).mouseover(function() {
+          $( "#pw" ).append( "<h5 id='passParagraph'> Password needs to be at least 7 characters long.</h5>" );
+        });
+
+        $( "img[class='p']" ).mouseout(function() {
+          $( "h5" ).remove();
+        });
     }
   });
 
