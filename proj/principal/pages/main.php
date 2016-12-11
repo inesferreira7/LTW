84c97a962292cc4a21847605abc01b5a99b6547e
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2016-12-11 04:01:03 +0000

    Working on principal

---
+++ proj/principal/pages/main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Foodaholics</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+    <script type="text/javascript" src="../js/main.js"></script>
+</head>
+<body>
+
+
+
+
+<div class="parallax"></div>
+<div id="container">
+    <div id="logo" >
+        <img src="../res/images/logo.png" class="logo" alt="Foodaholics" width="312" >
+    </div>
+    <div id="buttons">
+        <input id='login' type="submit" value="Log In" />
+        <input id='register' type="submit" value="Register" />
+    </div>
+
+</div>
+
+
+
+<div class="parallax"></div>
+
+
+
+</body>
+</html>
