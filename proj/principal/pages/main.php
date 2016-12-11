3d38bec621378271f9e26507edf7f486d0f23c66
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2016-12-11 15:25:50 +0000

    main page !

--- proj/principal/pages/main.php
+++ proj/principal/pages/main.php
@@ -1,3 +1,19 @@
+<?php
+
+include_once("connection.php");
+
+global $db;
+
+session_start();
+
+if(isset($_SESSION['id'])){
+    header('Location: principalUser.php');
+    die();
+    return;
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,28 +23,47 @@
     <link rel="stylesheet" href="../css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
     <script type="text/javascript" src="../js/main.js"></script>
+    
+
 </head>
 <body>
 
 
-
-
 <div class="parallax"></div>
+
 <div id="container">
     <div id="logo" >
         <img src="../res/images/logo.png" class="logo" alt="Foodaholics" width="312" >
     </div>
     <div id="buttons">
-        <input id='login' type="submit" value="Log In" />
-        <input id='register' type="submit" value="Register" />
+            <button id="login" onclick="document.getElementById('formContainer').style.display='block'">Login</button>
+            <input id='register' type="submit" value="Register" />
     </div>
-
 </div>
 
 
+<div id="formContainer" class="modal">
+
+    <form class="modal-content animate" action="action_page.php">
+
+        <div class="formPU">
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <input id="loginPU" type="submit" value="Log In"> </input>
+
+            <button id="cancelPU" type="button" onclick="document.getElementById('formContainer').style.display='none'" >Cancel</button>
+
+        </div>
+
+        </div>
+    </form>
+</div>
 
 <div class="parallax"></div>
-
 
 
 </body>
