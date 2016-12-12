88c2fd8747a95f799ed08c4db67a8adb5512ca95
Ines Ferreira <inesferreira789@gmail.com>
2016-12-12 11:57:39 +0000

    Added separate file for owner register

---
+++ proj/principal/pages/registerO.php
@@ -0,0 +1,15 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ 	<head>
+ 		<title>Register-Foodaholics</title>
+ 		<meta charset="UTF-8">
+ 		<link rel="stylesheet" href="../css/reset.css">
+ 		<link rel="stylesheet" href="../css/registerO.css">
+  </head>
+  <body>
+  </body>
+</html>
